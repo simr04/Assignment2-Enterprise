0b475ec898c72995120991b2a8b68e935efe9ed3
Simranjot Kaur <simranbrar0412@gmail.com>
2025-04-08 23:46:47 -0600

    Update App to include course name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>simran</h1>
-       
+        <h2>Enterprise Computing Course Software Development</h2>
       </header>
     </div>
   );
